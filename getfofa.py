85fca45a7cd1b220f6428e7a4e97878e22bcbe0a
M09ic <m09ician@gmail.com>
2021-07-22 17:03:45 +0800

    add domain

--- getfofa.py
+++ getfofa.py
@@ -8,5 +8,9 @@ def spider(c,domain):
 if __name__ == '__main__':
     c = FofaClient()
     res = spider(c,"mca.gov.cn")
+    for i in set(map(lambda x: x[3], res)):
+        print(i)
+    # for i in set():
+    #     print(i)
     for i in statCIDR(map(lambda x: x[1], res)).values():
         print(guessCIDR(i))
