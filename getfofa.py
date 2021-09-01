5f4fa13c9e6d18e1246ce2c0910a85bef8e61e40
M09ic <m09ician@gmail.com>
2021-09-01 10:50:36 +0800

    模块化

--- getfofa.py
+++ getfofa.py
@@ -1,121 +1,9 @@
 import json
-import logging
 import os
 import click
-import gevent
-from gevent import monkey
-from gevent.pool import Pool
 from functools import partial
-from queue import Queue
 
-monkey.patch_all()
-g = Pool(100)
-
-from utils import *
-from webtookit import *
-
-
-client = FofaClient()
-taskqueue = Queue()
-querys = set()
-
-
-def get_fofa(code):
-    if code not in querys and code != "":
-        logging.info("fofa querying "+code)
-        querys.add(code)
-        return client.query(code,isfilter=True)
-    else:
-        return []
-
-
-def cset(iter):
-    "remove blank elem"
-    s = set(iter)
-    if "" in iter:
-        s.remove("")
-    return s
-
-def combine_fofa_result(fd:FofaData, data):
-    """
-    :param data:
-    :return : different urls,ips,domains,icps,bool
-    :rtype : List[set],bool
-    """
-    if len(data) == 0:
-        return {},{},{},{},False
-    keys = ["url", "ip", "", "domain", "", "icp"]
-    return *[fd.union(keys[i],cset(v)) for i,v in enumerate(zip(*data)) if i !=2 and i != 4],True
-
-
-def check_fofatype(fofatype):
-    if isinstance(fofatype,list) or isinstance(fofatype,tuple):
-        for t in fofatype:
-            assert t in ["cert", "domain", "ip", "icon_hash", "cidr"], "%s fofa query type"
-    assert fofatype in ["cert", "domain", "ip", "icon_hash", "cidr"], "%s fofa query type"
-    return True
-
-
-
-# def run_fofas(fofatype, fofaquerys):
-#     for q in fofaquerys:
-#         code = get_fofaquery(fofatype, q)
-#         data = get_fofa(code)
-#         combine_fofa_result(fd, data)
-        # fofadata.union_fofa(ips, urls, domains, icps)
-
-
-def enqueue(k, targets, depth):
-    taskqueue.put((k,targets,depth))
-
-@CheckDepth
-def run_fofa(*args,fd,depth):
-    if len(args) == 1:
-        data = get_fofa(args[0])
-    elif len(args) > 1 :
-        key,targets = args
-        code = join_fofaqueries(**{key:targets})
-        data = get_fofa(code)
-    else:
-        return
-    urls, ips, domains, icps, ok = combine_fofa_result(fd, data) # 合并数据,返回新增的数据
-    if not ok: # 如果fofa无数据,则退出
-        return
-
-    # 获取ico,将得到的ico_hash加入到队列
-    enqueue("icon_hash",callback_ico(urls,fd),depth+1)
-
-    _, domains_icp = callback_icp(icps,fd) # 通过icp获取ip与domain
-    domains.union(domains_icp)
-    enqueue("domain",domains,depth+1)  # 将domains以domain加入到队列 # 将domains以cert加入到队列
-    enqueue("cert",domains,depth+1)
-
-def run(code):
-    tmpfd = FofaData(True,logging.info)
-    # tmpfd.merge(fd)
-    run_fofa(code,fd=tmpfd)
-    while taskqueue.qsize() > 0:
-        k,targets,depth = taskqueue.get()
-        if targets:
-            run_fofa(k,targets,fd=tmpfd,depth=depth)
-
-    return tmpfd
-
-
-def callback_ico(urls,fd):
-    icojobs = [g.spawn(get_hash,url) for url in urls]
-    gevent.joinall(icojobs)
-    icos = filter_ico(icojobs)
-    fd.unions(ico=icos)
-    return icos
-
-
-def callback_icp(icps,fd):
-    icpjobs = [g.spawn(Beian.get_host, icp) for icp in icps]
-    gevent.joinall(icpjobs)
-    ips,domains = sort_doaminandip(filter_icp(icpjobs))
-    fd.unions(ip=ips, domain=domains)
-    return ips,domains
+from lib import *
 
 
 def loadfile(filename):
@@ -214,5 +102,6 @@ def main(code,filename,output,fd:FofaData):
 
 if __name__ == '__main__':
     # command()
+
     command()
     # pool.waitall()
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .iputil import *
-from .util import *
-from .fofadata import FofaData
-from .Depth import *
