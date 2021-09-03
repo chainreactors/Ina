60eb328e323cb253b16a9cbc2f49a59beb5d7895
M09ic <m09ician@gmail.com>
2021-09-03 16:45:58 +0800

    优化代码结构

--- getfofa.py
+++ getfofa.py
@@ -1,21 +1,17 @@
 from gevent import monkey
-# monkey.patch_all()
-import json
-import os
-import click
-import logging
-from functools import partial
+monkey.patch_all()
 
-from lib import *
+from getfofa import *
 from webtookit import *
-
-
+import click,json,os,logging
+from functools import partial
 
 
 def loadfile(filename):
-    f = open(filename,"r",encoding="utf-8")
+    f = open(filename, "r", encoding="utf-8")
     s = f.read()
     return loadjson(s)
+
 
 def loadjson(s):
     try:
@@ -36,16 +32,15 @@ def getcode(code):
         return ""
 
 
-
 @click.command()
-@click.option("--code","-c",help="单条语句搜索,不递归爬取,无法与其他语句合并")
-@click.option("--filename","-f",help="输出文件名")
-@click.option("--output","-o",default="ip,domain,cidr")
-def command(code,filename,output):
-    main(code,filename,output,FofaData(False))
+@click.option("--code", "-c", help="单条语句搜索,不递归爬取,无法与其他语句合并")
+@click.option("--filename", "-f", help="输出文件名")
+@click.option("--output", "-o", default="ip,domain,cidr")
+def command(code, filename, output):
+    main(code, filename, output)
 
 
-def main(code,filename,output,fd:FofaData):
+def main(code, filename, output):
     fofa = Fofa(FofaClient())
 
     if filename:
@@ -55,9 +50,9 @@ def main(code,filename,output,fd:FofaData):
 
     if code:
         data = fofa.get_fofa(code)
-        fofa.combine_fofa_result(fd,data)
+        fofa.combine_fofa_result(fofa.fd, data)
         print()
-        fd.outputdata(["ico","ip","icp","url","domain","cidr"],outfunc=outfunc)
+        fofa.fd.outputdata(["ico", "ip", "icp", "url", "domain", "cidr"], outfunc=outfunc)
         exit(0)
 
     fds = {}
@@ -70,48 +65,47 @@ def main(code,filename,output,fd:FofaData):
             else:
                 continue
         index += 1
-        tmpfd = fofa.run(fofacode,fd)
-        fds[index] = (fofacode,tmpfd)
-        if sum([len(i) for i in fd.diffs_fd(tmpfd)]):
+        tmpfd = fofa.run(fofacode)
+        fds[index] = (fofacode, tmpfd)
+        if sum([len(i) for i in fofa.fd.diffs_fd(tmpfd)]):
             logging.info("found new assets:")
-            (tmpfd - fd).outputdata(output.split(","),outfunc=outfunc)
+            (tmpfd - fofa.fd).outputdata(output.split(","), outfunc=outfunc)
         else:
             logging.info("not found new asset")
 
-        while out := click.prompt("choice output(ip,cidr,ico,icp,url,domain) or enter [help], [c|continue], [exit], [diff], [merge], [to_file <filename>]"):
+        while out := click.prompt(
+                "choice output(ip,cidr,ico,icp,url,domain) or enter [help], [c|continue], [exit], [diff], [merge], [to_file <filename>]"):
             if out == "exit":
                 exit()
             elif out == "":
                 continue
-            elif out in ["continue","c"]: # 如果输入continue,则爬下一条fofa语句
+            elif out in ["continue", "c"]:  # 如果输入continue,则爬下一条fofa语句
                 break
             elif out == "diff":
                 logging.info("print new assets different from fd")
-                (tmpfd - fd).outputdata(outfunc=outfunc)
+                (tmpfd - fofa.fd).outputdata(outfunc=outfunc)
             elif "merge" in out:
                 if len(v := out.split(" ")) == 1:
-                    fd.merge(tmpfd)
+                    fofa.fd.merge(tmpfd)
                 else:
-                    fd.merge(fds[v][1])
+                    fofa.fd.merge(fds[v][1])
                 logging.info("merge success")
             elif "to_file" in out:
                 outs = out.split(" ")
                 if len(outs) >= 2 or filename:
                     tmpfilename = filename if filename else outs[-1]
-                    printfunc= partial(write2file,filename=tmpfilename)
-                    fd.outputdata(outfunc=printfunc)
+                    printfunc = partial(write2file, filename=tmpfilename)
+                    fofa.fd.outputdata(outfunc=printfunc)
                 else:
                     print("please input filename,example: to_file out.txt")
                     continue
             elif out == "help":
                 pass
             elif out == "history":
-                [print(i,v[0]) for i,v in fds.items()]
+                [print(i, v[0]) for i, v in fds.items()]
             else:
-                fd.outputdata(out.split(","),outfunc=print)
+                fofa.fd.outputdata(out.split(","), outfunc=print)
 
 
 if __name__ == '__main__':
-    # command()
     command()
-    # pool.waitall()
