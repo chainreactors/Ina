3f9b9d305921e35e8cf286596c16f2af3d303514
M09ic <m09ician@gmail.com>
2021-10-28 17:40:33 +0800

    添加example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+from gevent import monkey
+monkey.patch_all()
+
+from getfofa import *
+
+
+if __name__ == '__main__':
+    fofa = Fofa()
+    fd = fofa.run('ip="47.95.116.67"')
+    fd.outputdata()
